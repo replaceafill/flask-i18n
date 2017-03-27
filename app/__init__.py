c9a5e247217b33787d35ba16731107b8f18b7e0d
Douglas Cerna <douglascerna@yahoo.com>
2017-03-27 15:01:59 -0600

    Added endpoint redirects

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import redirect
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import url_for
 from flask_babel import Babel
+from werkzeug.routing import BuildError
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -17,9 +19,24 @@ def get_locale():
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/2')
+def two():
+    return render_template('two.html')
+
+@app.route('/3')
+def three():
+    return render_template('three.html')
+
 @app.route('/lang/<language>')
 def select_language(language):
-    response = redirect('/')
+    url = '/'
+    endpoint = request.args.get('camefrom')
+    if endpoint is not None:
+        try:
+            url = url_for(endpoint)
+        except BuildError:
+            pass
+    response = redirect(url)
     if language in SUPPORTED_LANGUAGES:
         response.set_cookie('LANGUAGE', language)
     return response
